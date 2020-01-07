b37cc14f26932f69dce4b42b85a04dc5cfe0ed2c
Falk Pappert <falk.pappert@unibw.de>
2020-01-07 15:11:39 +0100

    updated duration object tests to junit5

--- de.terministic.fabsim/src/test/java/de/terministic/fabsim/tests/durationobjecttests/ExponentialDurationObjectTest.java
+++ de.terministic.fabsim/src/test/java/de/terministic/fabsim/tests/durationobjecttests/ExponentialDurationObjectTest.java
@@ -1,11 +1,9 @@
 package de.terministic.fabsim.tests.durationobjecttests;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
 import java.util.Random;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import de.terministic.fabsim.core.duration.ExponentialDurationObject;
 
@@ -14,20 +12,20 @@ public class ExponentialDurationObjectTest {
 	@Test
 	public void sampleTestOneDay() {
 		ExponentialDurationObject obj = new ExponentialDurationObject(86400L, new Random());
-		long sum=0L;
+		long sum = 0L;
 		int sampleSize = 10000000;
-		for (int i=0;i<sampleSize;i++){
-			sum+=obj.getDuration();
+		for (int i = 0; i < sampleSize; i++) {
+			sum += obj.getDuration();
 		}
-		double avg = (sum*1.0)/sampleSize;
-		assertEquals(86400.0,avg,100);
+		double avg = (sum * 1.0) / sampleSize;
+		Assertions.assertEquals(86400.0, avg, 100);
 	}
 
 	@Test
 	public void avgTestOneDay() {
 		ExponentialDurationObject obj = new ExponentialDurationObject(86400L, new Random());
 		double avg = obj.getAvgDuration();
-		assertEquals(86400.0,avg,0);
+		Assertions.assertEquals(86400.0, avg, 0);
 	}
 
 }
