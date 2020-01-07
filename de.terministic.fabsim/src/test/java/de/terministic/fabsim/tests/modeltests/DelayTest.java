86020c3e1654d80e47758f50005510de04ded1e5
Falk Pappert <falk.pappert@unibw.de>
2020-01-07 15:25:02 +0100

    updated model tests to junit5

--- de.terministic.fabsim/src/test/java/de/terministic/fabsim/tests/modeltests/DelayTest.java
+++ de.terministic.fabsim/src/test/java/de/terministic/fabsim/tests/modeltests/DelayTest.java
@@ -1,10 +1,9 @@
 package de.terministic.fabsim.tests.modeltests;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 
 import de.terministic.fabsim.components.BasicRouting;
 import de.terministic.fabsim.components.ProcessStep;
@@ -22,7 +21,7 @@ import de.terministic.fabsim.statistics.FirstCycleTimeTracker;
 import de.terministic.fabsim.statistics.FlowItemCounter;
 
 public class DelayTest {
-	
+
 	FabModel model;
 	SimulationEngine engine;
 	Source source;
@@ -31,48 +30,48 @@ public class DelayTest {
 	ProcessStep tgStep1;
 	ProcessStep tgStep2;
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		model = new FabModel();
-		Sink sink = (Sink)model.getSimComponentFactory().createSink();
-		toolGroup=(ToolGroup) model.getSimComponentFactory().createToolGroup("Toolgroup1");
-		toolGroup2=(ToolGroup) model.getSimComponentFactory().createToolGroup("Toolgroup2");
+		Sink sink = (Sink) model.getSimComponentFactory().createSink();
+		toolGroup = (ToolGroup) model.getSimComponentFactory().createToolGroup("Toolgroup1");
+		toolGroup2 = (ToolGroup) model.getSimComponentFactory().createToolGroup("Toolgroup2");
 		Recipe recipe = model.getSimComponentFactory().createRecipe("Recipe1");
-		tgStep1 = model.getSimComponentFactory().createProcessStepAndAddToRecipe("Step1", toolGroup, 2L, ProcessType.LOT,recipe);
-		tgStep2 = model.getSimComponentFactory().createProcessStepAndAddToRecipe("Step2", toolGroup2, 3L, ProcessType.LOT,recipe);
-		model.getSimComponentFactory().createProcessStepAndAddToRecipe("Step2", sink, 0L, ProcessType.LOT,recipe);
-		Product product=model.getSimComponentFactory().createProduct("Product", recipe);
+		tgStep1 = model.getSimComponentFactory().createProcessStepAndAddToRecipe("Step1", toolGroup, 2L,
+				ProcessType.LOT, recipe);
+		tgStep2 = model.getSimComponentFactory().createProcessStepAndAddToRecipe("Step2", toolGroup2, 3L,
+				ProcessType.LOT, recipe);
+		model.getSimComponentFactory().createProcessStepAndAddToRecipe("Step2", sink, 0L, ProcessType.LOT, recipe);
+		Product product = model.getSimComponentFactory().createProduct("Product", recipe);
 
-		source = (Source)model.getSimComponentFactory().createSource("Source1", product, 1L);
+		source = (Source) model.getSimComponentFactory().createSource("Source1", product, 1L);
 
-		EventListManager eventList= new EventListManager();
+		EventListManager eventList = new EventListManager();
 		engine = new SimulationEngine(eventList);
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() throws Exception {
 	}
 
-
 	@Test
 	public void countTestWithTwoMachines() {
-		FlowItemCounter counter = new FlowItemCounter(); 
+		FlowItemCounter counter = new FlowItemCounter();
 		engine.init(model);
 		engine.addListener(counter);
 		engine.runSimulation(25L); // 25 runs, so 25 lots
-		assertEquals(25L, counter.getItemCount()); //in 25 runs are 25 lots generated
+		Assertions.assertEquals(25L, counter.getItemCount()); // in 25 runs are 25 lots generated
 	}
-	
 
 	@Test
 	public void processStepTimeTestWithTwoMachines() {
-		//FlowItemCounter counter = new FlowItemCounter(); 
-		//engine.init(model);
-		//engine.addListener(counter);
-		//engine.runSimulation(10L); 
-		assertEquals(2L, tgStep1.getDuration(null)); // test if the delay of machine 1 is 2 seconds
+		// FlowItemCounter counter = new FlowItemCounter();
+		// engine.init(model);
+		// engine.addListener(counter);
+		// engine.runSimulation(10L);
+		Assertions.assertEquals(2L, tgStep1.getDuration(null)); // test if the delay of machine 1 is 2 seconds
 	}
-	
+
 	@Test
 	public void cycleTimeTestOneJobWithTwoMachines() {
 		source.setCreateFirstAtTimeZero(true);
@@ -80,38 +79,37 @@ public class DelayTest {
 		FirstCycleTimeTracker cycleTime = new FirstCycleTimeTracker();
 		engine.init(model);
 		engine.addListener(cycleTime);
-		engine.runSimulation(7L); 
-		assertEquals(5L, cycleTime.getCycleTime());
-		
+		engine.runSimulation(7L);
+		Assertions.assertEquals(5L, cycleTime.getCycleTime());
+
 	}
-	
+
 	@Test
 	public void cycleTimeChangeDurationWithTwoMachines() {
 		FirstCycleTimeTracker cycleTime = new FirstCycleTimeTracker();
-		//set the delays of the machines
-		long durationStep1 = 7L ; long durationStep2 = 11L;
-		tgStep1.setDuration(model.getDurationObjectFactory().createConstantDurationObject(durationStep1));  
+		// set the delays of the machines
+		long durationStep1 = 7L;
+		long durationStep2 = 11L;
+		tgStep1.setDuration(model.getDurationObjectFactory().createConstantDurationObject(durationStep1));
 		tgStep2.setDuration(model.getDurationObjectFactory().createConstantDurationObject(durationStep2));
 		engine.init(model);
-		engine.addListener(cycleTime);  
-		engine.runSimulation(200L);          // run time of simulation should not matter
-		assertEquals((durationStep1+durationStep2), cycleTime.getCycleTime());
+		engine.addListener(cycleTime);
+		engine.runSimulation(200L); // run time of simulation should not matter
+		Assertions.assertEquals((durationStep1 + durationStep2), cycleTime.getCycleTime());
 //		System.out.println(durationStep1+durationStep2);
 	}
 
 	@Test
 	public void countSinkTestWithTwoMachines() {
-		FlowItemCounter counter = new FlowItemCounter(); 
+		FlowItemCounter counter = new FlowItemCounter();
 //		FlowItemDestructionEvent destruct = new FlowItemDestructionEvent();
-	    FirstCycleTimeTracker cycleTime = new FirstCycleTimeTracker();
-	   	engine.init(model);
+		FirstCycleTimeTracker cycleTime = new FirstCycleTimeTracker();
+		engine.init(model);
 		engine.addListener(counter);
 		engine.runSimulation(5L);
-	//	System.out.println( cycleTime.getCycleTime());
-		assertEquals(5L, counter.getItemCount());  // in 5 sec are 5 lots generated
-	//	assertEquals(1L, cycleTime.processEvent()); // in 5 sec is only 1 lot to sink
+		// System.out.println( cycleTime.getCycleTime());
+		Assertions.assertEquals(5L, counter.getItemCount()); // in 5 sec are 5 lots generated
+		// assertEquals(1L, cycleTime.processEvent()); // in 5 sec is only 1 lot to sink
 	}
-	
+
 }
-
-
--- de.terministic.fabsim/src/test/java/de/terministic/fabsim/tests/modeltests/GG1ModelTest.java
+++ de.terministic.fabsim/src/test/java/de/terministic/fabsim/tests/modeltests/GG1ModelTest.java
@@ -1,23 +1,20 @@
 package de.terministic.fabsim.tests.modeltests;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import de.terministic.fabsim.components.BasicRouting;
 import de.terministic.fabsim.components.ProcessStep;
+import de.terministic.fabsim.components.ProcessStep.ProcessType;
 import de.terministic.fabsim.components.Product;
 import de.terministic.fabsim.components.Recipe;
 import de.terministic.fabsim.components.Sink;
 import de.terministic.fabsim.components.Source;
-import de.terministic.fabsim.components.ProcessStep.ProcessType;
 import de.terministic.fabsim.components.equipment.ToolGroup;
 import de.terministic.fabsim.core.EventListManager;
 import de.terministic.fabsim.core.FabModel;
 import de.terministic.fabsim.core.SimulationEngine;
-import de.terministic.fabsim.core.duration.ConstantDurationObject;
 import de.terministic.fabsim.statistics.FirstCycleTimeTracker;
 
 public class GG1ModelTest {
@@ -27,55 +24,55 @@ public class GG1ModelTest {
 	Source source;
 	ToolGroup toolGroup;
 	ProcessStep tgStep;
-	
-	@Before
+
+	@BeforeEach
 	public void setUp() throws Exception {
 
-		
 		model = new FabModel();
-		Sink sink = (Sink)model.getSimComponentFactory().createSink();
-		toolGroup=(ToolGroup) model.getSimComponentFactory().createToolGroup("Toolgroup");
+		Sink sink = (Sink) model.getSimComponentFactory().createSink();
+		toolGroup = (ToolGroup) model.getSimComponentFactory().createToolGroup("Toolgroup");
 		Recipe recipe = model.getSimComponentFactory().createRecipe("Recipe1");
-		tgStep = model.getSimComponentFactory().createProcessStepAndAddToRecipe("Step1", toolGroup, 5L, ProcessType.LOT,recipe);
-		model.getSimComponentFactory().createProcessStepAndAddToRecipe("Step2", sink, 0L, ProcessType.LOT,recipe);
-		Product product=model.getSimComponentFactory().createProduct("Product", recipe);
+		tgStep = model.getSimComponentFactory().createProcessStepAndAddToRecipe("Step1", toolGroup, 5L, ProcessType.LOT,
+				recipe);
+		model.getSimComponentFactory().createProcessStepAndAddToRecipe("Step2", sink, 0L, ProcessType.LOT, recipe);
+		Product product = model.getSimComponentFactory().createProduct("Product", recipe);
 
-		source = (Source)model.getSimComponentFactory().createSource("Source1", product, 10L);
+		source = (Source) model.getSimComponentFactory().createSource("Source1", product, 10L);
 
-		EventListManager eventList= new EventListManager();
+		EventListManager eventList = new EventListManager();
 		engine = new SimulationEngine(eventList);
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() throws Exception {
 		model = null;
-		engine= null;
-		toolGroup=null;
-		tgStep=null;
-		source=null;
+		engine = null;
+		toolGroup = null;
+		tgStep = null;
+		source = null;
 	}
 
 	@Test
 	public void cycleTimeTestWithZeroProcessingTime() {
 		FirstCycleTimeTracker tracker = new FirstCycleTimeTracker();
-		long duration =0L;
+		long duration = 0L;
 		tgStep.setDuration(model.getDurationObjectFactory().createConstantDurationObject(duration));
 		engine.init(model);
 		engine.addListener(tracker);
 //		engine.addListener(new SimulationLog());
 		engine.runSimulation(30L);
-		assertEquals(duration, tracker.getCycleTime());
+		Assertions.assertEquals(duration, tracker.getCycleTime());
 	}
 
 	@Test
 	public void cycleTimeTestWithNonZeroProcessingTime() {
 		FirstCycleTimeTracker tracker = new FirstCycleTimeTracker();
-		long duration =5L;
+		long duration = 5L;
 		tgStep.setDuration(model.getDurationObjectFactory().createConstantDurationObject(duration));
 		engine.init(model);
 		engine.addListener(tracker);
 		engine.runSimulation(30L);
-		assertEquals(duration, tracker.getCycleTime());
+		Assertions.assertEquals(duration, tracker.getCycleTime());
 	}
 
 }
