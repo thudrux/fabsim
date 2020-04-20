1b2e29f461ee13f96b1656d893bc6ef742a3dafc
Falk Pappert <falk.pappert@unibw.de>
2020-04-20 20:59:52 +0200

    corrected test to use new default EventListManager

--- de.terministic.fabsim/src/test/java/de/terministic/fabsim/tests/runtimetests/EmptyFabRuntimeTest.java
+++ de.terministic.fabsim/src/test/java/de/terministic/fabsim/tests/runtimetests/EmptyFabRuntimeTest.java
@@ -1,106 +1,77 @@
-//package de.terministic.fabsim.tests.runtimetests;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//import de.terministic.fabsim.components.ProcessStep.ProcessType;
-//import de.terministic.fabsim.components.Product;
-//import de.terministic.fabsim.components.Recipe;
-//import de.terministic.fabsim.components.Sink;
-//import de.terministic.fabsim.core.ComponentComparator;
-//import de.terministic.fabsim.core.ComponentGroupedEventListManager;
-//import de.terministic.fabsim.core.EventListTypeManager;
-//import de.terministic.fabsim.core.FabModel;
-//import de.terministic.fabsim.core.PriorityQueueEventListManager;
-//import de.terministic.fabsim.core.SimulationEngine;
-//import de.terministic.fabsim.core.TimeGroupedEventListManager;
-//import de.terministic.fabsim.core.TreeSetEventListManager;
-//
-//public class EmptyFabRuntimeTest {
-//
-//	private FabModel buildModel() {
-//		FabModel model = new FabModel();
-//		Sink sink = (Sink) model.getSimComponentFactory().createSink();
-//		Recipe recipe = model.getSimComponentFactory().createRecipe("Recipe1");
-//		model.getSimComponentFactory().createProcessStepAndAddToRecipe("lastStep", sink, 0L, ProcessType.LOT, recipe);
-//		Product product = model.getSimComponentFactory().createProduct("Product", recipe);
-//		model.getSimComponentFactory().createSource("Source1", product, 10L);
-//		return model;
-//	}
-//
-//	@Test
-//	public void runTimeForEmptyModelWithTimeGroupedEventListManagerTest() {
-//		long startTime = System.currentTimeMillis();
-//		for (int i = 0; i < 10; i++) {
-//			FabModel model = buildModel();
-//			TimeGroupedEventListManager eventList = new TimeGroupedEventListManager();
-//			SimulationEngine engine = new SimulationEngine(eventList);
-//			engine.init(model);
-//			engine.runSimulation(10000000L);
-//		}
-//		long duration = System.currentTimeMillis() - startTime;
-//
-//		Assertions.assertTrue(duration < 5000);
-//	}
-//
-//	@Test
-//	public void runTimeForEmptyModelWithTypeGroupedEventListManagerTest() {
-//		long startTime = System.currentTimeMillis();
-//		for (int i = 0; i < 10; i++) {
-//			FabModel model = buildModel();
-//			EventListTypeManager eventList = new EventListTypeManager(new ComponentComparator());
-//			SimulationEngine engine = new SimulationEngine(eventList);
-//			engine.init(model);
-//			engine.runSimulation(10000000L);
-//		}
-//		long duration = System.currentTimeMillis() - startTime;
-//
-//		Assertions.assertTrue(duration < 5000);
-//	}
-//
-//	@Test
-//	public void runTimeForEmptyModelWithComponentGroupedEventListManagerTest() {
-//		long startTime = System.currentTimeMillis();
-//		for (int i = 0; i < 10; i++) {
-//			FabModel model = buildModel();
-//			ComponentGroupedEventListManager eventList = new ComponentGroupedEventListManager();
-//			SimulationEngine engine = new SimulationEngine(eventList);
-//			engine.init(model);
-//			engine.runSimulation(10000000L);
-//		}
-//		long duration = System.currentTimeMillis() - startTime;
-//
-//		Assertions.assertTrue(duration < 5000);
-//	}
-//
-//	@Test
-//	public void runTimeForEmptyModelWithPriorityQueueEventListManagerTest() {
-//		long startTime = System.currentTimeMillis();
-//		for (int i = 0; i < 10; i++) {
-//			FabModel model = buildModel();
-//			PriorityQueueEventListManager eventList = new PriorityQueueEventListManager();
-//			SimulationEngine engine = new SimulationEngine(eventList);
-//			engine.init(model);
-//			engine.runSimulation(10000000L);
-//		}
-//		long duration = System.currentTimeMillis() - startTime;
-//
-//		Assertions.assertTrue(duration < 5000);
-//	}
-//
-//	@Test
-//	public void runTimeForEmptyModelWithTreeSetEventListManagerTest() {
-//		long startTime = System.currentTimeMillis();
-//		for (int i = 0; i < 10; i++) {
-//			FabModel model = buildModel();
-//			TreeSetEventListManager eventList = new TreeSetEventListManager();
-//			SimulationEngine engine = new SimulationEngine(eventList);
-//			engine.init(model);
-//			engine.runSimulation(10000000L);
-//		}
-//		long duration = System.currentTimeMillis() - startTime;
-//
-//		Assertions.assertTrue(duration < 5000);
-//	}
-//
-//}
+package de.terministic.fabsim.tests.runtimetests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import de.terministic.fabsim.components.ProcessStep.ProcessType;
+import de.terministic.fabsim.components.Product;
+import de.terministic.fabsim.components.Recipe;
+import de.terministic.fabsim.components.Sink;
+import de.terministic.fabsim.core.ComponentGroupedEventListManager;
+import de.terministic.fabsim.core.FabModel;
+import de.terministic.fabsim.core.PriorityQueueEventListManager;
+import de.terministic.fabsim.core.SimulationEngine;
+import de.terministic.fabsim.core.TreeSetEventListManager;
+
+public class EmptyFabRuntimeTest {
+
+	private FabModel buildModel() {
+		FabModel model = new FabModel();
+		Sink sink = (Sink) model.getSimComponentFactory().createSink();
+		Recipe recipe = model.getSimComponentFactory().createRecipe("Recipe1");
+		model.getSimComponentFactory().createProcessStepAndAddToRecipe("lastStep", sink, 0L, ProcessType.LOT, recipe);
+		Product product = model.getSimComponentFactory().createProduct("Product", recipe);
+		model.getSimComponentFactory().createSource("Source1", product, 10L);
+		return model;
+	}
+
+	@Test
+	@Tag("slow")
+	public void runTimeForEmptyModelWithComponentGroupedEventListManagerTest() {
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < 10; i++) {
+			FabModel model = buildModel();
+			ComponentGroupedEventListManager eventList = new ComponentGroupedEventListManager();
+			SimulationEngine engine = new SimulationEngine(eventList);
+			engine.init(model);
+			engine.runSimulation(10000000L);
+		}
+		long duration = System.currentTimeMillis() - startTime;
+
+		Assertions.assertTrue(duration < 5000);
+	}
+
+	@Test
+	@Tag("slow")
+	public void runTimeForEmptyModelWithPriorityQueueEventListManagerTest() {
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < 10; i++) {
+			FabModel model = buildModel();
+			PriorityQueueEventListManager eventList = new PriorityQueueEventListManager();
+			SimulationEngine engine = new SimulationEngine(eventList);
+			engine.init(model);
+			engine.runSimulation(10000000L);
+		}
+		long duration = System.currentTimeMillis() - startTime;
+
+		Assertions.assertTrue(duration < 5000);
+	}
+
+	@Test
+	@Tag("slow")
+	public void runTimeForEmptyModelWithTreeSetEventListManagerTest() {
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < 10; i++) {
+			FabModel model = buildModel();
+			TreeSetEventListManager eventList = new TreeSetEventListManager();
+			SimulationEngine engine = new SimulationEngine(eventList);
+			engine.init(model);
+			engine.runSimulation(10000000L);
+		}
+		long duration = System.currentTimeMillis() - startTime;
+
+		Assertions.assertTrue(duration < 5000);
+	}
+
+}
