8abf60468f11124f49d58321b89338c5aecdd44b
thudrux <falk.pappert@unibw.de>
2022-08-25 17:15:09 +0200

    added FFLogger

---
+++ de.terministic.fabsim/src/main/java/de/terministic/fabsim/statistics/FlowFactorLogger.java
@@ -0,0 +1,45 @@
+package de.terministic.fabsim.statistics;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.terministic.fabsim.components.FlowItemDestructionEvent;
+import de.terministic.fabsim.core.ISimEvent;
+import de.terministic.fabsim.core.SimEventListener;
+
+public class FlowFactorLogger extends SimEventListener {
+	private Logger logger = LoggerFactory.getILoggerFactory().getLogger(this.getClass().getSimpleName());
+	private long counter = 0L;
+
+	public long getCounter() {
+		return counter;
+	}
+
+	public void setCounter(long counter) {
+		this.counter = counter;
+	}
+
+	public double getFfSum() {
+		return ffSum;
+	}
+
+	public void setFfSum(double ffSum) {
+		this.ffSum = ffSum;
+	}
+
+	private double ffSum = 0L;
+
+	@Override
+	public void processEvent(final ISimEvent event) {
+		if (event instanceof FlowItemDestructionEvent) {
+			counter++;
+			double ct = ((FlowItemDestructionEvent) event).getEventTime() - event.getFlowItem().getCreationTime();
+			double rpt = event.getFlowItem().getRPT();
+			ffSum += ct / rpt;
+		}
+	}
+
+	public double getFF() {
+		return ffSum / (1.0 * counter);
+	}
+}
