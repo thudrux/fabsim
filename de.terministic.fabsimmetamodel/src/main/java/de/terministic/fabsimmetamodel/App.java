e65bcafcab997ab3b6b85bb9bd6931a2615e8c68
Falk Pappert <thudrux@gmail.com>
2024-01-13 10:50:33 +0100

    initial commit of FabSimMetaModel

---
+++ de.terministic.fabsimmetamodel/src/main/java/de/terministic/fabsimmetamodel/App.java
@@ -0,0 +1,13 @@
+package de.terministic.fabsimmetamodel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
