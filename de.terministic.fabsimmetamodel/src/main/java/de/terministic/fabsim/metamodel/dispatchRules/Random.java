971fbd7a837e5e06841583a98c6a20e7d845c4d9
Leon Becker <44971838+leonmbecker@users.noreply.github.com>
2026-07-01 12:03:24 +0200

    add dispatch rule random

---
+++ de.terministic.fabsimmetamodel/src/main/java/de/terministic/fabsim/metamodel/dispatchRules/Random.java
@@ -0,0 +1,48 @@
+package de.terministic.fabsim.metamodel.dispatchRules;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ThreadLocalRandom;
+
+import de.terministic.fabsim.metamodel.AbstractFlowItem;
+import de.terministic.fabsim.metamodel.components.equipment.BatchDetails;
+import de.terministic.fabsim.metamodel.components.equipment.queuecentriccontroller.FifoBatchFlowItemQueue;
+import de.terministic.fabsim.metamodel.components.equipment.queuecentriccontroller.FifoFlowItemQueue;
+import de.terministic.fabsim.metamodel.components.equipment.queuecentriccontroller.IFlowItemQueue;
+
+public class Random extends AbstractDispatchRule {
+
+	public Random() {
+		super("Random");
+	}
+
+	@Override
+	public AbstractFlowItem getBestItem(final ArrayList<AbstractFlowItem> items) {
+		if (items == null || items.isEmpty()) {
+			return null;
+		}
+		return items.get(ThreadLocalRandom.current().nextInt(items.size()));
+	}
+
+	@Override
+	public ArrayList<AbstractFlowItem> sortWithDispatchRule(final ArrayList<AbstractFlowItem> items) {
+		Collections.shuffle(items, ThreadLocalRandom.current());
+		return items;
+	}
+
+	@Override
+	public ArrayList<AbstractFlowItem> addItemToList(final AbstractFlowItem item, final ArrayList<AbstractFlowItem> items) {
+		items.add(item);
+		return items;
+	}
+
+	@Override
+	public IFlowItemQueue createBatchQueue(final BatchDetails details) {
+		return new FifoBatchFlowItemQueue(details);
+	}
+
+	@Override
+	public IFlowItemQueue createQueue() {
+		return new FifoFlowItemQueue();
+	}
+}
